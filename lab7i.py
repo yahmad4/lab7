1d4be53041b6ee7a7da598bc820649b1890f07f4
Yasir Ahmad <yahmad4@myseneca.ca>
2024-11-24 23:45:33 -0500

    Added lab7i.py

---
+++ lab7i.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Student ID: yahmad4
+
+def function1():
+    global schoolName
+    schoolName = 'SICT'
+    print('print() in function1 on schoolName:', schoolName)
+
+def function2():
+    global schoolName
+    schoolName = 'SSDO'
+    print('print() in function2 on schoolName:', schoolName)
+
+schoolName = 'Seneca'
+print('print() in main on schoolName:', schoolName)
+function1()
+print('print() in main on schoolName:', schoolName)
+function2()
+print('print() in main on schoolName:', schoolName)
