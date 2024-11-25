d19f58635c973854d45a291a1be3138b78cd06f0
Yasir Ahmad <yahmad4@myseneca.ca>
2024-11-24 23:31:39 -0500

    Updated lab7d.py

--- lab7d.py
+++ lab7d.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id] 
+# Student ID: yahmad4 
+
 class Time:
     """Simple object type for time of the day.
         data attributes: hour, minute, second
-        function attributes: __init__, __str__, __repr__
+        function attributes: __init__, __str__, __repr__,
                             time_to_sec, format_time,
                             change_time, sum_time
     """
-    def __init__(self,hour=12,minute=0,second=0):
+    def __init__(self, hour=12, minute=0, second=0):
         """constructor for time object""" 
         self.hour = hour
         self.minute = minute
@@ -18,11 +19,11 @@ class Time:
         return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
 
     def sum_times(self, t2):
-        """Add two time objests and return the sum."""
+        """Add two time objects and return the sum."""
         self_sec = self.time_to_sec()
         t2_sec = t2.time_to_sec()
-        sum = sec_to_time(self_sec + t2_sec)
-        return sum
+        sum_time = sec_to_time(self_sec + t2_sec)
+        return sum_time
 
     def change_time(self, seconds):
         time_seconds = self.time_to_sec()
@@ -39,7 +40,7 @@ class Time:
 
     def valid_time(self):
         """check for the validity of the time object attributes:
-        24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
+        0 <= hour < 24, 0 <= minute < 60, 0 <= second < 60 """
         if self.hour < 0 or self.minute < 0 or self.second < 0:
             return False
         if self.minute >= 60 or self.second >= 60 or self.hour >= 24:
